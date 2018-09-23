ab96dcc4eeba9471aab30c08d5bf08c457a120da
bghd <particuliers_cours@yahoo.fr>
2018-09-23 17:15:39 +0200

    j'ai ajouté le fichier"test" pour faire des tests ..

--- src/com/ocr/yassine/shophome/Product.java
+++ src/com/ocr/yassine/shophome/Product.java
@@ -1,4 +1,19 @@
 package com.ocr.yassine.shophome;
 
-public class Product {
+public class Product
+{
+    String name;
+    String description;
+    double price;
+
+    public void look()
+    {
+
+    }
+    public void buy(Bill bill, Integer quantity)
+    {
+
+    }
+
+
 }
---
+++ test/com/ocr/yassine/shophome/ProductTest.java
@@ -0,0 +1,7 @@
+package com.ocr.yassine.shophome;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+}
